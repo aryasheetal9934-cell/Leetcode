b3279a8e8a096aac13e3772293c8b9b6bf4b0a44
Sheetal Arya <aryasheetal9934@gmail.com>
2026-07-22 11:26:33 +0530

    Time: 0 ms (100%), Space: 21 MB (85.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
-        n = len(nums)
-        return nums[n // 2]
+        n=len(nums)
+        return nums[n//2]
