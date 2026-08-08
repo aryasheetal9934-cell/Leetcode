294e6d8c2ca2cb365253437af7fc436622ad8b17
Sheetal Arya <aryasheetal9934@gmail.com>
2026-08-08 12:55:02 +0530

    Time: 407 ms (33.77%), Space: 19.4 MB (20.5%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,37 +1,28 @@
 class Solution(object):
    def fourSum(self, nums, target):
-       """
-       :type nums: List[int]
-       :type target: int
-       :rtype: List[List[int]]
-       """
-       nums.sort()  # Sort the array
-       unique_quadruplets = set()
+
+       nums.sort()  
+       ans = set()
        n = len(nums)
 
-
-       # Pick the first and second elements
        for i in range(n - 3):
            for j in range(i + 1, n - 2):
                val = target - nums[i] - nums[j]
-               low, high = j + 1, n - 1
+               left, right = j + 1, n - 1
 
-
-               # Two-pointer approach for remaining two numbers
-               while low < high:
-                   total = nums[low] + nums[high]
-
+               while left < right:
+                   total = nums[left] + nums[right]
 
                    if total == val:
-                       unique_quadruplets.add((nums[i], nums[j], nums[low], nums[high]))
-                       low += 1
-                       high -= 1
+                       ans.add((nums[i], nums[j], nums[left], nums[right]))
+                       left += 1
+                       right -= 1
                    elif total < val:
-                       low += 1  # Increase sum
+                       left += 1  
                    else:
-                       high -= 1  # Decrease sum
+                       right -= 1 
 
 
-       return [list(quad) for quad in unique_quadruplets]  # Convert to list of lists
+       return [list(quad) for quad in ans]  
 
 
